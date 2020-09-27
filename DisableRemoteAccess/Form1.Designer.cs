3dc1c043e52ac9c1cab26d62ade94163e8988ad7
Yura Gaidaenko <y.gaidaenko@agrifirm.com>
2020-09-27 12:30:34 +0300

    Добавьте файлы проекта.

---
+++ DisableRemoteAccess/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace DisableRemoteAccess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(12, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 61);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Off";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.Blue;
+            this.button2.Location = new System.Drawing.Point(12, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 62);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "On";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(105, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 30);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Статус сервера";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(358, 145);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Бит Сервис. Контроль сервера.";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ DisableRemoteAccess/Form1.cs
@@ -0,0 +1,219 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using tik4net;
+using tik4net.Objects;
+using tik4net.Objects.Interface;
+using tik4net.Objects.Interface.Bridge;
+using tik4net.Objects.Ip.Firewall;
+using tik4net.Objects.Ip.Hotspot;
+using tik4net.Objects.System;
+using Color = System.Drawing.Color;
+
+namespace DisableRemoteAccess
+{
+    public partial class Form1 : Form 
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string HOST = "name.server.com";
+        public string USER = "apiuser";
+        public string PASS = "password";
+        public string route = "192.168.0.0/24";
+        public string nameRule = "Demo";
+
+        private void button1_Click(object sender, EventArgs e) 
+        {
+            try
+            {
+                using (ITikConnection connection = ConnectionFactory.CreateConnection(TikConnectionType.Api))
+                {
+
+                    connection.Open(HOST, USER, PASS);
+
+                    int i = 0;
+
+                    var natRule = connection.CreateCommandAndParameters("/ip/route/print", "dst-address", route).ExecuteList();
+                    var value = natRule.Count();
+                    if (value == i)
+                    {
+                        Color colorOff_n = Color.Red;
+                        label1.ForeColor = colorOff_n;
+                        label1.Text = ("Сервер уже отключен,\n      включите его.");
+                        return;
+                    }
+                }
+            }
+            catch
+            {
+                Color colorOff_n = Color.Red;
+                label1.ForeColor = colorOff_n;
+                label1.Text = ("Возникло исключение!\nИмя сервера или порт \nне доступты!");
+            }
+                
+            try
+            {
+                using (ITikConnection connection = ConnectionFactory.CreateConnection(TikConnectionType.Api))
+                {
+                    connection.Open(HOST, USER, PASS);
+
+                    var natRule = connection.CreateCommandAndParameters("/ip/address/print", "comment", nameRule).ExecuteList();
+                    var id = natRule.Single().GetId();
+
+                    var disableRule = connection.CreateCommandAndParameters("/ip/address/disable", TikSpecialProperties.Id, id);
+                    disableRule.ExecuteNonQuery();
+                    Color colorOff = Color.Red;
+                    label1.ForeColor = colorOff;
+                    label1.Text = ("Вы отключили сервер.");
+                }
+            }
+            catch
+            {
+                Color colorOff_n = Color.Red;
+                label1.ForeColor = colorOff_n;
+                label1.Text = ("Возникло исключение!\nИмя сервера или порт \nне доступты!");
+            }           
+        }       
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (ITikConnection connection = ConnectionFactory.CreateConnection(TikConnectionType.Api))
+                {
+                    connection.Open(HOST, USER, PASS);
+
+                    int n = 1;
+
+                    var natRule = connection.CreateCommandAndParameters("/ip/route/print", "dst-address", route).ExecuteList();
+                    var value = natRule.Count();
+                    if (value == n)
+                    {
+                        Color colorOn_n = Color.Blue;
+                        label1.ForeColor = colorOn_n;
+                        label1.Text = ("Сервер уже включен.");
+                        return;
+                    }
+                }
+            }
+            catch
+            {
+                Color colorOff_n = Color.Red;
+                label1.ForeColor = colorOff_n;
+                label1.Text = ("Возникло исключение!\nИмя сервера или порт \nне доступты!");
+            }
+
+            try
+            {
+                using (ITikConnection connection = ConnectionFactory.CreateConnection(TikConnectionType.Api))
+                {
+                    connection.Open(HOST, USER, PASS);
+
+                    int n = 1;
+
+                    var natRule = connection.CreateCommandAndParameters("/ip/route/print", "dst-address", route).ExecuteList();
+                    var value = natRule.Count();
+                    if (value == n)
+                    {
+                        Color colorOn_n = Color.Blue;
+                        label1.ForeColor = colorOn_n;
+                        label1.Text = ("Сервер уже включен.");
+                        return;
+                    }
+                }
+            }
+            catch
+            {
+                label1.Text = ("Возникло исключение");
+            }
+
+            try
+            {
+                using (ITikConnection connection = ConnectionFactory.CreateConnection(TikConnectionType.Api))
+                {
+
+                    connection.Open(HOST, USER, PASS);
+
+                    var natRule = connection.CreateCommandAndParameters("/ip/address/print", "comment", nameRule).ExecuteList();
+                    var id = natRule.Single().GetId();
+                    var enableRule = connection.CreateCommandAndParameters("/ip/address/enable", TikSpecialProperties.Id, id);
+                    enableRule.ExecuteNonQuery();
+
+                    Color colorOn = Color.Blue;
+                    label1.ForeColor = colorOn;
+                    label1.Text = ("Вы включили сервер.");
+                }
+            }
+            catch
+            {
+                Color colorOff_n = Color.Red;
+                label1.ForeColor = colorOff_n;
+                label1.Text = ("Возникло исключение!\nИмя сервера или порт \nне доступты!");
+            }           
+        } 
+        private void label1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (ITikConnection connection = ConnectionFactory.CreateConnection(TikConnectionType.Api))
+                {
+                    connection.Open(HOST, USER, PASS);
+
+                    int n = 1;
+
+                    var natRule = connection.CreateCommandAndParameters("/ip/route/print", "dst-address", route).ExecuteList();
+                    var value = natRule.Count();
+                    if (value == n)
+                    {
+                        Color colorOn = Color.Blue;
+                        label1.ForeColor = colorOn;
+                        label1.Text = ("Текущий статус сервера\n          ДОСТУПЕН!");
+                    }
+                }
+            }
+            catch
+            {
+                Color colorOff_n = Color.Red;
+                label1.ForeColor = colorOff_n;
+                label1.Text = ("Возникло исключение!\n Имя сервера или порт \nне доступты!");
+            }
+
+            try
+            {
+                using (ITikConnection connection = ConnectionFactory.CreateConnection(TikConnectionType.Api))
+                {
+                    connection.Open(HOST, USER, PASS);
+
+                    int i = 0;
+
+                    var natRule = connection.CreateCommandAndParameters("/ip/route/print", "dst-address", route).ExecuteList();
+                    var value = natRule.Count();
+                    if (value == i)
+                    {
+                        Color colorOff_n = Color.Red;
+                        label1.ForeColor = colorOff_n;
+                        label1.Text = ("Текущий статус сервера\n        НЕ ДОСТУПЕН!");
+                    }
+                }
+            }
+            catch
+            {
+                Color colorOff_n = Color.Red;
+                label1.ForeColor = colorOff_n;
+                label1.Text = ("Возникло исключение!\nИмя сервера или порт \nне доступты!");
+            }           
+        }
+    }
+}
