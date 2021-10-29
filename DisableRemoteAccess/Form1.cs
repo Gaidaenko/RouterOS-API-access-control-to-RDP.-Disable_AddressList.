a5c20425715a532750817a0b16e15ccc346c51fb
Yuiy <45212496+Gaidaenko@users.noreply.github.com>
2021-10-29 21:23:23 +0300

    Update Form1.cs

--- DisableRemoteAccess/Form1.cs
+++ DisableRemoteAccess/Form1.cs
@@ -1,15 +1,6 @@
 ﻿
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Net;
-using System.Net.NetworkInformation;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using tik4net;
 using tik4net.Objects;
